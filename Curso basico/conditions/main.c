31d3bfedc96a060115c5cc6fc665c24c2d3d085f
Margionet Fabiola Díaz <31838291+margiofabiolad@users.noreply.github.com>
2018-11-18 13:39:33 -0400

    Update main.c

--- Curso basico/conditions/main.c
+++ Curso basico/conditions/main.c
@@ -4,15 +4,17 @@
 int main()
 {
 int age;
+  
 printf("Please enter the age: ");
 scanf("%d", &age);
+  
 if (age > 18)  {
-printf("Age is greater than 18");
+  printf("Age is greater than 18");
 }
 if (age == 18) {
-printf("Age is equal to 18");
+  printf("Age is equal to 18");
 }
 if (age < 18) {
-printf("Age is less than 18");
+  printf("Age is less than 18");
 }
 }
